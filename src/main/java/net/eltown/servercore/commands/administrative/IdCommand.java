fc5d8e4c7a92ae430ea614a38d8bccae1139f6d6
Mundschutziii <37897528+Mundschutziii@users.noreply.github.com>
2022-01-30 16:00:51 +0100

    ELTOWN-6 added IdCommand.java

--- src/main/java/net/eltown/servercore/commands/administrative/IdCommand.java
+++ src/main/java/net/eltown/servercore/commands/administrative/IdCommand.java
@@ -1,4 +1,36 @@
 package net.eltown.servercore.commands.administrative;
 
-public class IdCommand {
+import net.eltown.servercore.ServerCore;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+public class IdCommand extends Command {
+
+    private final ServerCore serverCore;
+
+    public IdCommand(final ServerCore serverCore) {
+        super("id");
+        this.serverCore = serverCore;
+        this.setPermission("core.command.id");
+    }
+
+    @Override
+    public boolean execute(@NotNull CommandSender sender, @NotNull String s, @NotNull String[] args) {
+        if (!sender.hasPermission(this.getPermission())) return true;
+        if (sender instanceof Player player) {
+            final ItemStack itemStack = player.getInventory().getItemInMainHand();
+
+            if (itemStack.getType() == Material.AIR) {
+                player.sendMessage("§8» §fCore §8| §7Bitte halte ein Item in deiner Hand.");
+                return true;
+            }
+
+            player.sendMessage("§8» §fCore §8| §7Das Item in deiner Hand heißt: §a" + itemStack.getType().name());
+        }
+        return true;
+    }
 }
