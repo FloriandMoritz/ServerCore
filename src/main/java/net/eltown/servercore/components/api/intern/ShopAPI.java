1635c563d6cd4ee78a99718a77128354458e032b
Mundschutziii <37897528+Mundschutziii@users.noreply.github.com>
2022-02-01 21:11:09 +0100

    ELTOWN-52 used Queues

--- src/main/java/net/eltown/servercore/components/api/intern/ShopAPI.java
+++ src/main/java/net/eltown/servercore/components/api/intern/ShopAPI.java
@@ -2,6 +2,7 @@ package net.eltown.servercore.components.api.intern;
 
 import net.eltown.servercore.ServerCore;
 import net.eltown.servercore.components.data.shops.ShopCalls;
+import net.eltown.servercore.components.tinyrabbit.Queue;
 
 import java.util.function.BiConsumer;
 
@@ -12,25 +13,25 @@ public record ShopAPI(ServerCore serverCore) {
             final double buy = Double.parseDouble(delivery.getData()[1]);
             final double sell = Double.parseDouble(delivery.getData()[2]);
             callback.accept(buy * amount, sell * amount);
-        }, "api.shops.callback", ShopCalls.REQUEST_ITEM_PRICE.name(), namespaceId);
+        }, Queue.SHOPS_CALLBACK, ShopCalls.REQUEST_ITEM_PRICE.name(), namespaceId);
     }
 
     public void getMinBuySell(final String namespaceId, final BiConsumer<Double, Double> callback) {
         this.serverCore.getTinyRabbit().sendAndReceive((delivery) -> {
             callback.accept(Double.parseDouble(delivery.getData()[1]), Double.parseDouble(delivery.getData()[2]));
-        }, "api.shops.callback", ShopCalls.REQUEST_MIN_BUY_SELL.name(), namespaceId);
+        }, Queue.SHOPS_CALLBACK, ShopCalls.REQUEST_MIN_BUY_SELL.name(), namespaceId);
     }
 
     public void sendBought(final String namespaceId, final int amount) {
-        this.serverCore.getTinyRabbit().send("api.shops.receive", ShopCalls.UPDATE_ITEM_BOUGHT.name(), namespaceId, amount + "");
+        this.serverCore.getTinyRabbit().send(Queue.SHOPS_RECEIVE, ShopCalls.UPDATE_ITEM_BOUGHT.name(), namespaceId, amount + "");
     }
 
     public void sendSold(final String namespaceId, final int amount) {
-        this.serverCore.getTinyRabbit().send("api.shops.receive", ShopCalls.UPDATE_ITEM_SOLD.name(), namespaceId, amount + "");
+        this.serverCore.getTinyRabbit().send(Queue.SHOPS_RECEIVE, ShopCalls.UPDATE_ITEM_SOLD.name(), namespaceId, amount + "");
     }
 
     public void setPrice(final String namespaceId, final double price) {
-        this.serverCore.getTinyRabbit().send("api.shops.receive", ShopCalls.UPDATE_ITEM_PRICE.name(), namespaceId, price + "");
+        this.serverCore.getTinyRabbit().send(Queue.SHOPS_RECEIVE, ShopCalls.UPDATE_ITEM_PRICE.name(), namespaceId, price + "");
     }
 
 }
